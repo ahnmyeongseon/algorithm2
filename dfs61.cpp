33b1c0ccb7b08378da81baab1e1c2006bfd523da
gohardsun <ahnmyungsun95@gmail.com>
2021-04-22 00:43:48 +0900

    dfs 이용하여 특정 수 만들기

--- dfs61.cpp
+++ dfs61.cpp
@@ -5,15 +5,25 @@
 using namespace std;
 int n, m, cnt=0;
 int pr[11];
+int lt[5];
 
+// 특정 수가 될 때의 경로찍기
 void dfs(int a, int sum){
     if(a == n+1){
-        if(sum == m){ cnt++;}
+        if(sum == m){
+            cnt++;
+            for(int i=1; i<=n; i++){
+                cout<<lt[i]<<" ";
+            }}
         return;
     }
     else{
+        lt[a] = +pr[a];
         dfs(a+1, sum+pr[a]);
+        lt[a] = -pr[a];
         dfs(a+1, sum-pr[a]);
+        lt[a] = 0;
+        dfs(a+1, sum);
     }
 }
 
@@ -25,7 +35,8 @@ int main(){
     }
     dfs(1, 0);
 
-    cout<<cnt<<"";
+    if(cnt==0) cout<<"-1";
+    else cout<<cnt<<"";
 
     return 0;
 }
